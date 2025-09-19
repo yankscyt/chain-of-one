921a2f289be3f902b426769a094ac6928e9fe914
yankscyt <yankscaburnay@gmail.com>
2025-09-19 12:52:23 +0800

    Added complete project structure with NOTES, LICENSE, .gitignore, and main.py

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Welcome to Chain of One: Blockchain Carbon Credit Tracker")
+print("🌍 Welcome to Chain of One: Blockchain Carbon Credit Tracker")
+print("This is a prototype script. Future versions will integrate Cardano smart contracts.")
